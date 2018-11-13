44ca8cc8ad50769e0670c0e956a009cbe4eb8676
PovDima <povdima19@gmail.com>
2018-11-14 00:27:22 +0200

    init commit

---
+++ public/precache-manifest.1e3cd9ca09fa346da1ddee5ae915cacb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b15ef94746031f8853af907909881f2a",
+    "url": "/static/media/logo.b15ef947.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4bed7b81ee1bde8f2722",
+    "url": "/static/js/main.4bed7b81.chunk.js"
+  },
+  {
+    "revision": "cf5be8d71f67ac242686",
+    "url": "/static/js/1.cf5be8d7.chunk.js"
+  },
+  {
+    "revision": "4bed7b81ee1bde8f2722",
+    "url": "/static/css/main.a9d4376f.chunk.css"
+  },
+  {
+    "revision": "542828d0b84a7b32bec6e81fa515a29a",
+    "url": "/index.html"
+  }
+];
